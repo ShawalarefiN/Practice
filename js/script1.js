8f371423c4977e290570ee855b14d6df39d4add3
ShawalarefiN <shawalarefin132@gmail.com>
2025-10-15 22:42:20 +0600

    more problems added

--- js/script1.js
+++ js/script1.js
@@ -6,7 +6,7 @@ class person {
         this.age = age;        
     }
     introduce(){
-        console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`); 
+        console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.\n`); 
     }
 }
 const person1 = new person("Shawal", 20);
@@ -22,6 +22,34 @@ class rectangle {
         this.height = height;        
     }
     area(){
-         
+        let area = this.width * this.height;
+        return `Area = ${area}\n`
+    }
+    perimeter(){
+        let perimeter = 2 * (this.width + this.height);
+        return `Perimeter = ${perimeter}\n`
     }
 }
+
+const rect1 = new rectangle(20,10);
+console.log(rect1.area())
+console.log(rect1.perimeter())
+
+//Problem 3
+class car {
+    constructor(brand, model, year){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        }
+    getInfo(){
+        return `Brand = ${this.brand}\nmodel = ${this.model}\nyear = ${this.year}\n`
+    }
+        
+}
+const car1 = new car("toyota","gt86", 2016)
+
+console.log(car1.getInfo())
+
+//problem 4: Inheritance
+
