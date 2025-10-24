653d3040f4c692864e71346c8b24d11670696a7b
ShawalarefiN <shawalarefin132@gmail.com>
2025-10-24 23:50:57 +0600

    2 problem solved

--- js/script1.js
+++ js/script1.js
@@ -233,4 +233,21 @@ const acc2 = new BankAccount(1000)
 console.log(BankAccount.compareAccounts(acc1,acc2))
 
 //Problem 7: Temperature class
+class Temperature {
+  constructor(celcius){
+    this._celcius = celcius;
+  }
+  get ferenheit(){
+    return (this._celcius * 9/5) + 32; 
+  }
+  set ferenheit(f){
+    this._celcius = (f - 32) * 5/9;
+  }
+}
+const temp = new Temperature(0);
+console.log(temp.ferenheit);
 
+temp.ferenheit = 212;
+console.log(temp._celcius);
+
+//problem 8: user class
