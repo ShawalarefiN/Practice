9e9a1e0f7f592d6f316119b3e14dca92c44c37de
ShawalarefiN <shawalarefin132@gmail.com>
2025-10-25 19:24:48 +0600

    some remainings

--- js/librarySystem.js
+++ js/librarySystem.js
@@ -1,34 +1,34 @@
-class Book{
-    constructor(title, author){
-        this.title = title;
-        this.author = author;        
-    }
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 class Library {
-    constructor(){
-        this.books = [];
+  constructor() {
+    this.books = [];
+  }
+  addBook(book) {
+    this.books.push(book);
+  }
+  searchBook(title) {
+    const book = this.books.find((b) => b.title === title);
+    return book || `no book found`;
+  }
+  removeBook(title) {
+    this.books = this.books.filter((b) => b.title !== title);
+  }
+  showAllBooks() {
+    if (this.books.length === 0) {
+      console.log("No books in the library");
+    } else {
+      this.books.forEach((b) => console.log(`${b.title} by ${b.author}`));
     }
-    addBook(book){
-        this.books.push(book);
-    }
-    searchBook(title){
-        const book = this.books.find(b => b.title === title)      
-        return book || `no book found`
-    }
-    removeBook(title){
-        this.books = this.books.filter(b => b.title !== title);
-    }
-    showAllBooks(){
-        if (this.books === 0){
-            return `no books in the library`
-        } else {
-            return `${this.books.map(b => `${b.title} by ${b.author}\n`)}`
-        }
-    }
+  }
 }
 const lib = new Library();
 lib.addBook(new Book("1984", "Orwell"));
 lib.addBook(new Book("Dune", "Frank Herbert"));
 
-console.log(lib.searchBook("Dune")); 
-console.log(lib.showAllBooks()); 
+console.log(lib.searchBook("Dune"));
+lib.showAllBooks();
