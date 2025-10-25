f793366fe16beeb8240b76d12344fe982a61d9bc
ShawalarefiN <shawalarefin132@gmail.com>
2025-10-25 14:58:02 +0600

    static method

--- js/script1.js
+++ js/script1.js
@@ -251,3 +251,17 @@ temp.ferenheit = 212;
 console.log(temp._celcius);
 
 //problem 8: user class
+class User {
+  static count = 0;
+  constructor (name){
+    this.name = name;
+    User.count++;
+  }
+}
+const user1 = new User("Alice");
+const user2 = new User("Bob");
+const user3 = new User("Charlie");
+
+console.log(User.count); 
+
+//problem 9: Product discount system
