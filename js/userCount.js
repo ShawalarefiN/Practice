aefe625af81d2dd39e25aa11f5da5b8c440e1794
ShawalarefiN <shawalarefin132@gmail.com>
2025-10-25 17:33:38 +0600

    library system problem

--- js/userCount.js
+++ js/userCount.js
@@ -11,3 +11,21 @@ const user2 = new User("Bob");
 const user3 = new User("Charlie");
 
 console.log(User.count);
+
+//Employee Counter
+class Employee {
+  static count = 0;
+  constructor(name,position){
+    this._name = name;
+    this._position = position;
+    Employee.count++;
+  }
+  static showCount(){   
+    return `\nTotal employees: ${Employee.count}`
+  }    
+}
+
+const e1 = new Employee("Alice", "Manager");
+const e2 = new Employee("Bob", "Developer");
+
+console.log(Employee.showCount()); 
