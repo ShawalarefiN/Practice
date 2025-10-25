67b96ed19122866490f22205bdd9bb8161b97330
ShawalarefiN <shawalarefin132@gmail.com>
2025-10-25 17:52:42 +0600

    additional methods added in library system

--- js/librarySystem.js
+++ js/librarySystem.js
@@ -1,12 +1,34 @@
 class Book{
-    constructor(title, author, year){
+    constructor(title, author){
         this.title = title;
-        this.author = author;
-        this.year = year;
+        this.author = author;        
     }
 }
 class Library {
     constructor(){
-        this.book = [];
+        this.books = [];
+    }
+    addBook(book){
+        this.books.push(book);
+    }
+    searchBook(title){
+        const book = this.books.find(b => b.title === title)      
+        return book || `no book found`
+    }
+    removeBook(title){
+        this.books = this.books.filter(b => b.title !== title);
+    }
+    showAllBooks(){
+        if (this.books === 0){
+            return `no books in the library`
+        } else {
+            return `${this.books.map(b => `${b.title} by ${b.author}\n`)}`
+        }
     }
 }
+const lib = new Library();
+lib.addBook(new Book("1984", "Orwell"));
+lib.addBook(new Book("Dune", "Frank Herbert"));
+
+console.log(lib.searchBook("Dune")); 
+console.log(lib.showAllBooks()); 
